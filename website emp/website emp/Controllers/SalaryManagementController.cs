736b137d87acc278ff16e2240edc33098cafe5f4
Farman Kaskar <ismail02091995@gmail.com>
2020-01-13 06:15:04 -0800

    Changes Made by ismail with in salary Module

---
+++ website emp/website emp/Controllers/SalaryManagementController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website_emp.Controllers
+{
+    public class SalaryManagementController : Controller
+    {
+        // GET: SalaryManagement
+        public ActionResult AddTemplate()
+        {
+            return View();
+        }
+        public ActionResult makepayment()
+        {
+            return View();
+        }
+        public ActionResult AssignSalary()
+        {
+            return View();
+        }
+        public ActionResult CheckSalary()
+        {
+            return View();
+        }
+        public ActionResult PayrollManagement()
+        {
+            return View();
+        }
+    }
+}
