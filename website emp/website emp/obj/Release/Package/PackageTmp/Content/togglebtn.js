34ace490fa6f03c03c5f688584641d21bd4dc341
Farman Kaskar <ismail02091995@gmail.com>
2020-01-15 06:57:48 -0800

    changes made by ismail

---
+++ website emp/website emp/obj/Release/Package/PackageTmp/Content/togglebtn.js
@@ -0,0 +1,17 @@
+﻿
+var count = 1;
+function ftn()
+{
+    var togglebar = document.getElementById("togglebar");
+    var togglex = document.getElementById("togglex");
+    if (count === 1) {
+        togglebar.style.display = "none";
+        togglex.style.display = "block";
+        count++;
+    }
+    else {
+        togglebar.style.display = "block";
+        togglex.style.display = "none";
+        count = 1;
+    }
+}
