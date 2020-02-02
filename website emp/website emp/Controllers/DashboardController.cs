c8aa963e415b3c0d35d9c86df5015cc0314b0663
Asad Mehmood <Asad7121208@hotmail.com>
2020-02-02 14:40:55 +0500

    hunter

--- website emp/website emp/Controllers/DashboardController.cs
+++ website emp/website emp/Controllers/DashboardController.cs
@@ -9,14 +9,13 @@ namespace website_emp.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
-        website_emp.Models.context db = new Models.context();
         public ActionResult Index()
         {
-            Models.status statusid = db.States.FirstOrDefault(p => p.State == "Allowed");
-            var employ = db.Users.Where(p=>p.Status.StatusId == statusid.StatusId).Select(p=>p).ToArray();
-            ViewBag.AttendanceNumber = employ.Count();
-            var departments = db.Departments.Select(p => p).ToArray();
-            ViewBag.DepartmentsNumber = departments.Count();
+            //Models.status statusid = db.States.FirstOrDefault(p => p.State == "Allowed");
+            //var employ = db.Users.Where(p=>p.Status.StatusId == statusid.StatusId).Select(p=>p).ToArray();
+            //ViewBag.AttendanceNumber = employ.Count();
+            //var departments = db.Departments.Select(p => p).ToArray();
+            //ViewBag.DepartmentsNumber = departments.Count();
             return View();
         }
     }
